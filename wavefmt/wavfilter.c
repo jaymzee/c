be5a6955d81fb34fb82a99a9e1543ea8b081ad2f
jaymzee <jaymzee@users.noreply.github.com>
2019-02-11 15:02:56 -0500

    wavfilter - fixed up circular_state and reverb works correctly

--- wavefmt/wavfilter.c
+++ wavefmt/wavfilter.c
@@ -5,14 +5,14 @@
 
 struct circular_state {
     double *w;
-    double *a_indx;
     double *a_val;
-    double *b_indx;
     double *b_val;
-    int offset;
-    int N;
+    int *a_indx;
+    int *b_indx;
     int Na;
     int Nb;
+    int N;
+    int offset;
 };
 
 struct circular_state *
@@ -22,6 +22,8 @@ circular_create(const int N, int Nb, int *b_indx, double *b_val,
     struct circular_state *s;
     s = malloc(sizeof(struct circular_state));
     s->N = N;
+    s->Na = Na;
+    s->Nb = Nb;
     s->w = calloc(N, sizeof(double));
     s->a_indx = calloc(Na, sizeof(int));
     s->a_val  = calloc(Na, sizeof(double));
@@ -31,6 +33,7 @@ circular_create(const int N, int Nb, int *b_indx, double *b_val,
     memcpy(s->a_val,  a_val,  Na * sizeof(double));
     memcpy(s->b_indx, b_indx, Nb * sizeof(int));
     memcpy(s->b_val,  b_val,  Nb * sizeof(double));
+    s->offset = 0;
     return s;
 }
 
@@ -115,7 +118,7 @@ float circular_filter(float x, void *state)
     return y;
 }
 
-int test_canonical(int argc, char *argv[])
+int test_canonical_filter(int argc, char *argv[])
 {
     double a[3] = {1.0, 0.3, 0.3};
     double b[3] = {0.2, 0.2, 0.2};
@@ -126,31 +129,29 @@ int test_canonical(int argc, char *argv[])
         return -1;
     }
 
-    return wavefmt_filter(argv[1], argv[2], canonical_filter, can, WAVEFMT_FLOAT, 4.0); 
+    return wavefmt_filter(argv[1], argv[2], canonical_filter, can,
+                          WAVEFMT_FLOAT, 4.0); 
 }
 
-int test_circular(int argc, char *argv[])
+int test_reverb_filter(int argc, char *argv[])
 {
-    int    b_indx[2] = {1000, 2000};
+    int    b_indx[2] = {1, 3500};
     double b_val[2]  = {0.4, 0.4};
-    int    a_indx[2] = {1000, 2000};
-    double a_val[2]  = {0.4, 0.4};
-    struct circular_state *circ = circular_create(3000, 2, b_indx, b_val,
+    int    a_indx[1] = {3000};
+    double a_val[1]  = {0.6};
+    struct circular_state *circ = circular_create(5000, 2, b_indx, b_val,
                                                         2, a_indx, a_val);
 
     if (argc != 3) {
         fprintf(stderr, "Usage: filter infile outfile\n");
         return -1;
     }
-
-    return wavefmt_filter(argv[1], argv[2], circular_filter, circ, WAVEFMT_FLOAT, 4.0); 
+    
+    return wavefmt_filter(argv[1], argv[2], circular_filter, circ,
+                          WAVEFMT_FLOAT, 4.0); 
 }
 
 int main(int argc, char *argv[])
 {
-#ifndef CAN    
-    return test_canonical(argc, argv); 
-#else
-    return test_circular(argc, argv);
-#endif
+    return test_reverb_filter(argc, argv);
 }
