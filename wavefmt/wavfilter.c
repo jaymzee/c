1b3e8cc66246744aa8a1bf6d3cdd870182d15223
jaymzee <jaymzee@users.noreply.github.com>
2019-02-11 09:34:14 -0500

    wavfilter - state variables

--- wavefmt/wavfilter.c
+++ wavefmt/wavfilter.c
@@ -1,22 +1,39 @@
 #include "wavefmt.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+struct filterstate {
+    double w[5];
+};
 
 float filter(float x, void *state)
 {
-    return x;
+    struct filterstate *fs = state;
+    double *w = fs->w;
+    int N = (sizeof fs->w) / (sizeof *fs->w);
+    double y;
+
+    y = 0.5 * w[4] + 0.5 * x;
+
+    w[4] = w[3];
+    w[3] = w[2];
+    w[2] = w[1];
+    w[1] = w[0];
+    w[0] = x;
+
+    return y;
 }
 
 int main(int argc, char *argv[])
 {
-    int x;
+    struct filterstate *fs = calloc(1, sizeof(struct filterstate));
 
     if (argc != 3) {
         fprintf(stderr, "Usage: filter infile outfile\n");
         return -1;
     }
 
-    /* wavefmt_filter(argv[1], argv[2], filter, &x, WAVEFMT_PCM, 0.0); */
-    wavefmt_filter(argv[1], argv[2], filter, &x, WAVEFMT_FLOAT, 1.5); 
+    wavefmt_filter(argv[1], argv[2], filter, fs, WAVEFMT_FLOAT, 4.0); 
 
     return 0;
 }
