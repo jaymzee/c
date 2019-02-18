17cca07745928579669fa5e8fd216d07ad0bdc2c
jaymzee <jaymzee@users.noreply.github.com>
2019-02-18 10:12:03 -0500

    wavefmt - wavflanger

--- wavefmt/wavflanger.c
+++ wavefmt/wavflanger.c
@@ -1,34 +1,51 @@
 #include "wavefmt.h"
 #include "fracdelay.h"
 #include <stdio.h>
+#include <math.h>
 
+#define PI 3.141592654
+
+struct flanger
+{
+    double rate;
+    double phase;
+    struct fracdelay_state *delay;
+};
 
 float flanger_procsamp(float x, void *state)
 {
-    struct fracdelay_state *fs = state;
-    const int N = fs->N;
-    double *w = fs->w;
+    struct flanger *fl = state;
+    struct fracdelay_state *delay = fl->delay;
+    const int N = delay->N;
+    double *w = delay->w;
     double y;
-    double n = 900.5;
+    double n;
+    
+    n = N * (0.5 * cos(2 * PI * fl->rate * fl->phase) + 0.5);
     
     w[0] = x;
 
-    y = x + fracdelay_w(fs, n);
+    y = 0.5 * x + 0.5 * fracdelay_w(delay, n);
+
+    fracdelay_dec(delay);
+    fl->phase += 1.0 / 44100.0;
 
     return y;
 }
 
 int main(int argc, char *argv[])
 {
-    struct fracdelay_state *fs;
-    
-    fs = fracdelay_create(1000);
+    struct flanger fl;
+
+    fl.rate = 1.0;
+    fl.phase = 0.0;
+    fl.delay = fracdelay_create(1000);
 
     if (argc != 3) {
         fprintf(stderr, "Usage: wavflanger infile outfile\n");
         return -1;
     }
 
-    return wavefmt_filter(argv[1], argv[2], flanger_procsamp, fs,
+    return wavefmt_filter(argv[1], argv[2], flanger_procsamp, &fl,
                           WAVEFMT_PCM, 0.0); 
 }
