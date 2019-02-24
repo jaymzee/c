4d62e383832350a58df6830f2b33f6a9ec4c7b42
jaymzee <jaymzee@users.noreply.github.com>
2019-02-24 14:10:25 -0500

    wavefmt - comments

--- wavefmt/Delay.cpp
+++ wavefmt/Delay.cpp
@@ -1,9 +1,18 @@
 #include "Delay.h"
 
-Delay::Delay(int N) : w(N), offset(0)
+/*
+ * Delay::Delay() - construct a fractional delay line 
+ * @length: length of delay line
+ */
+Delay::Delay(int length) : w(length), offset(0)
 {
 }
 
+/*
+ * Delay::operator--() - advance delay line by one sample
+ *
+ * Return: reference to *this
+ */
 Delay& Delay::operator--()
 {
     if (--offset < 0)
@@ -11,6 +20,11 @@ Delay& Delay::operator--()
     return *this;
 }
 
+/*
+ * Delay::operator--() - retreat delay line by one sample
+ *
+ * Return: reference to *this
+ */
 Delay& Delay::operator++()
 {
     const int N = w.size();
@@ -19,6 +33,14 @@ Delay& Delay::operator++()
     return *this;
 }
 
+/*
+ * Delay::operator[](double) - interpolate w[n] while handling wrapping
+ * @n: index into delay line, must be >= 0 but may have a fractional part
+ *
+ * since there is a fractional part to n, linearly interpolate the samples
+ *
+ * Return: w[n]
+ */
 double Delay::operator[](double n)
 {
     double w1, w2, f;
@@ -31,11 +53,20 @@ double Delay::operator[](double n)
     return (1.0 - f) * w1 + f * w2;
 }
 
+/*
+ * Delay::operator[](int) - reference w[n] while handling wrapping
+ * @n: index into delay line
+ *
+ * Return: reference to w[n]
+ */
 double& Delay::operator[](int n)
 {
     return w[(offset + n) % w.size()];
 }
 
+/*
+ * Delay::length() - length of delay line
+ */
 int Delay::length()
 {
     return w.size();
