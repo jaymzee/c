84ae28b646ac5c734d825585cd7027dab5faf3ee
jaymzee <jaymzee@users.noreply.github.com>
2020-07-19 16:03:52 -0400

    bbrot - constants

--- mandelbrot/bb/bbrot.c
+++ mandelbrot/bb/bbrot.c
@@ -9,6 +9,23 @@ const double Y0 = -1.75;  /* Y is the real axis aligned vertically */
 const double Y1 = 0.75;
 const double X0 = -1.50; /* X is the imaginary axis aligned horizontally */
 const double X1 = 1.50;
+
+/* ESC_MAG for conventional mandelbrot rendering escape magnitude is usually 2.0, 
+   (values outside this will always escape to infinity) but with buddhabrot method 
+   setting this to 2.3 with the coordinates above avoids a harsh circle at the top edge (neg real axis) 
+
+   things to try
+
+   POINTS 10000000
+   MAX_ITER 1000
+
+   POINTS 5000000
+   MAX_ITER 10000
+*/
+const double ESC_MAG = 2.3;
+const int POINTS = 5000000;
+const int MAX_ITER = 10000;
+
 int count[W][H];
 
 void orbit()
@@ -16,17 +33,17 @@ void orbit()
     complex double c;
     complex double z;
 
-    for (int n = 0; n < 10000000; n++) {
+    for (int n = 0; n < POINTS; n++) {
         c = ((double)rand() / RAND_MAX * (Y1 - Y0) + Y0) +
             ((double)rand() / RAND_MAX * (X1 - X0) + X0)*I;
         z = 0;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < MAX_ITER; i++) {
             z = z * z + c;
-            if (cabs(z) > 2.0) {
+            if (cabs(z) > ESC_MAG) {
                 /* this c escaped so let's reiterate the sequence 
                    recording counts this time */
                 z = 0;
-                while (cabs(z) <= 2.0) {
+                while (cabs(z) <= ESC_MAG) {
                     int yy = (creal(z) - Y0) / (Y1 - Y0) * H;
                     int xx = (cimag(z) - X0) / (X1 - X0) * W;
                     if (xx >= 0 && xx < W && yy >= 0 && yy < H) {
