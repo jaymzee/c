da768b31415fa1543bb0730013a49b19c9cbcfe8
jaymzee <jaymzee@users.noreply.github.com>
2020-07-19 16:35:48 -0400

    bbrot - clipping will be done by another program
    rotated axis back to the original orientation, rotation
    will also be done by another program

--- mandelbrot/bb/bbrot.c
+++ mandelbrot/bb/bbrot.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <complex.h>
 
-#define W 1280
-#define H 1024
+#define IMG_WIDTH  1024
+#define IMG_HEIGHT 1280
 
-const double Y0 = -1.75;  /* Y is the real axis aligned vertically */
-const double Y1 = 0.75;
-const double X0 = -1.50; /* X is the imaginary axis aligned horizontally */
-const double X1 = 1.50;
+const double X0 = -1.75;    /* X is the real axis aligned horizontally */
+const double X1 =  0.75;
+const double Y0 = -1.50;    /* Y is the imaginary axis aligned vertically */
+const double Y1 =  1.50;
 
 /* ESC_MAG for conventional mandelbrot rendering escape magnitude is usually 2.0, 
    (values outside this will always escape to infinity) but with buddhabrot method 
@@ -23,10 +23,10 @@ const double X1 = 1.50;
    MAX_ITER 10000
 */
 const double ESC_MAG = 2.3;
-const int POINTS = 5000000;
-const int MAX_ITER = 10000;
+const int POINTS = 10000000;
+const int MAX_ITER = 1000;
 
-int count[W][H];
+int count[IMG_HEIGHT][IMG_WIDTH];
 
 void orbit()
 {
@@ -34,8 +34,8 @@ void orbit()
     complex double z;
 
     for (int n = 0; n < POINTS; n++) {
-        c = ((double)rand() / RAND_MAX * (Y1 - Y0) + Y0) +
-            ((double)rand() / RAND_MAX * (X1 - X0) + X0)*I;
+        c = ((double)rand() / RAND_MAX * (X1 - X0) + X0) +
+            ((double)rand() / RAND_MAX * (Y1 - Y0) + Y0) * I;
         z = 0;
         for (int i = 0; i < MAX_ITER; i++) {
             z = z * z + c;
@@ -44,10 +44,11 @@ void orbit()
                    recording counts this time */
                 z = 0;
                 while (cabs(z) <= ESC_MAG) {
-                    int yy = (creal(z) - Y0) / (Y1 - Y0) * H;
-                    int xx = (cimag(z) - X0) / (X1 - X0) * W;
-                    if (xx >= 0 && xx < W && yy >= 0 && yy < H) {
-                        count[xx][yy]++;
+                    /* map complex z back to image coordinates */
+                    int x = (creal(z) - X0) / (X1 - X0) * IMG_WIDTH;
+                    int y = (cimag(z) - Y0) / (Y1 - Y0) * IMG_HEIGHT;
+                    if (x >= 0 && x < IMG_WIDTH && y >= 0 && y < IMG_HEIGHT) {
+                        count[y][x]++;
                     }
                     z = z * z + c;
                 }
@@ -57,26 +58,13 @@ void orbit()
     }
 }
 
-void clip(int threshold)
-{
-    int i, j;
-    for (i = 0; i < H; i++) {
-        for (j = 0; j < W; j++) {
-            double c = count[j][i] / (double)threshold;
-            if (c > 1.0)
-                c = 1.0;
-            count[j][i] = c * 255.0;
-        }
-    }
-}
-
 void write_image()
 {
     printf("P2\n");
-    printf("%d %d\n255\n", W, H);
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            printf("%03d ", count[j][i]);
+    printf("%d %d\n255\n", IMG_WIDTH, IMG_HEIGHT);
+    for (int i = 0; i < IMG_HEIGHT; i++) {
+        for (int j = 0; j < IMG_WIDTH; j++) {
+            printf("%03d ", count[i][j]);
         }
         printf("\n");
     }
@@ -84,12 +72,7 @@ void write_image()
 
 int main(int argc, char *argv[])
 {
-    int clip_th = 255;
-    if (argc == 2) {
-        clip_th = atoi(argv[1]);
-    }
     orbit();
-    clip(clip_th);
     write_image();
 
     return 0;
