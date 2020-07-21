f4b7b75dab1a7e31407989881f751d95767c67bb
jaymzee <jaymzee@users.noreply.github.com>
2020-07-21 01:08:44 -0400

    bbrot - progress bar for bbrot

--- mandelbrot/bb/bbrot.c
+++ mandelbrot/bb/bbrot.c
@@ -32,34 +32,42 @@ struct image {
 
 void render_orbits(const struct image *img, int samples, int max_iter)
 {
-    complex double z, c;
     int *const buf = img->buffer;
     const int w = img->width;
     const int h = img->height;
+    const int chunk = samples / 100;
+    complex double z, c;
 
+    fprintf(stderr, "redering     ");
     for (int n = 0; n < samples; n++) {
+        /* display progress bar */
+        if (chunk > 0 && (n % chunk == 0)) {
+            fprintf(stderr, "\b\b\b\b%3d%%", (int)(100.0 * n / samples));
+        }
+        /* get a random sample point for c */
         c = ((double)rand() / RAND_MAX * (X1 - X0) + X0) +
             ((double)rand() / RAND_MAX * (Y1 - Y0) + Y0) * I;
+        /* find out if z escapes to infinity for this c */
         z = 0;
         for (int i = 0; i < max_iter; i++) {
             z = z * z + c;
             if (cabs(z) > ESCAPE_MAG) {
-                /* this c escaped so let's reiterate the sequence 
-                   recording counts this time */
+                /* this c escaped so reiterate the sequence but write orbit to image this time */
                 z = 0;
                 while (cabs(z) <= ESCAPE_MAG) {
+                    z = z * z + c;
                     /* map complex z back to image coordinates */
                     int x = (creal(z) - X0) / (X1 - X0) * w;
                     int y = (cimag(z) - Y0) / (Y1 - Y0) * h;
                     if (x >= 0 && x < w && y >= 0 && y < h) {
                         buf[y * w + x]++;
                     }
-                    z = z * z + c;
                 }
                 break;
             }
         }
     }
+    fprintf(stderr, "\b\b\b\b100%%\n");
 }
 
 void write_image(const struct image *img, int samples, int max_iter)
