2dadd29650e03763638fedf40cea40d1d3806707
jaymzee <jaymzee@users.noreply.github.com>
2020-08-18 09:14:57 -0400

    learn-c mmap.c

---
+++ learn-c/mmap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    const char *fname = "foo.bin";
+    const int fd = open(fname, O_RDWR);
+    char *p;
+
+    if (fd < 0) {
+        perror(fname);
+        exit(1);
+    }
+
+    //p = mmap(NULL, 256, PROT_READ, MAP_PRIVATE, fd, 0);
+    p = mmap(NULL, 256, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("%02X\n", 0xFF & p[0]);
+    p[0] = 42;
+    munmap(p, 256);
+    close(fd);
+}
