a266b554b01c80140a9802a4e5c8b78f8363df9a
jaymzee <jaymzee@users.noreply.github.com>
2021-10-09 12:36:18 -0400

    k&r malloc (2nd edition)

--- alloc/kr/main.c
+++ alloc/kr/main.c
@@ -6,81 +6,87 @@
 
 typedef struct header { // free block header
     struct header *ptr;  // next free block
-    size_t size; // size of this free block
+    size_t size; // size of this free block in units
 } HEADER;
 
-static HEADER base;             // empty list to get started
-static HEADER *allocp = NULL;   // last allocated block
+static HEADER base;            // empty list to get started
+static HEADER *freep = NULL;   // start of free list
 
-void ffree(void *ap)
+void kr_free(void *ap)
 {
-    HEADER *p, *q;
+    HEADER *bp, *p;
 
-    p = (HEADER *)ap - 1;   // point to header
-    for (q = allocp; !(p > q && p < q->ptr); q=q->ptr) {
-        if (q >= q->ptr && (p > q || p < q->ptr)) {
-            break;  // at one end or other
+    bp = (HEADER *)ap - 1;   // point to block header
+    for (p = freep; !(bp > p && bp < p->ptr); p=p->ptr) {
+        if (p >= p->ptr && (bp > p || bp < p->ptr)) {
+            break;  // freed block at start or end of arena
         }
     }
 
-    if  (p+p->size == q->ptr) { // join to upper nbr
-        p->size += q->ptr->size;
-        p->ptr = q->ptr->ptr;
+    if  (bp + bp->size == p->ptr) { // join to upper nbr
+        bp->size += p->ptr->size;
+        bp->ptr = p->ptr->ptr;
     } else {
-        p->ptr = q->ptr;
+        bp->ptr = p->ptr;
     }
 
-    if (q+q->size == p) {
-        q->size += p->size;
-        q->ptr = p->ptr;
+    if (p + p->size == bp) {
+        p->size += bp->size;
+        p->ptr = bp->ptr;
     } else {
-        q->ptr = p;
+        p->ptr = bp;
     }
-    allocp = q;
+    freep = p;
 }
 
+/*
+ * ask system for more memory
+ */
 static HEADER *morecore(size_t nu)
 {
     void *cp;
     HEADER *up;
     size_t rnu;
 
-    rnu = NALLOC * ((nu+NALLOC-1) / NALLOC);
-    cp = sbrk(rnu * sizeof(HEADER));
+    if (nu < NALLOC) {
+        nu = NALLOC;
+    }
+    //rnu = NALLOC * ((nu+NALLOC-1) / NALLOC);
+    cp = sbrk(nu * sizeof(HEADER));
     if (cp < 0) {
         return NULL;
     }
     up = (HEADER *)cp;
-    up->size = rnu;
-    ffree(up+1);
+    up->size = nu;
+    kr_free(up+1);
 
-    return allocp;
+    return freep;
 }
 
-void *alloc(size_t nbytes)
+void *kr_malloc(size_t nbytes)
 {
-    HEADER *p, *q;
+    HEADER *p, *prevp;
     size_t nunits;
 
-    nunits = 1 + (nbytes + sizeof(HEADER)-1) / sizeof(HEADER);
-    if ((q = allocp) == NULL) { // no free list yet
-        base.ptr = allocp = q = &base;
+    nunits = 1 + (nbytes + sizeof(HEADER) - 1) / sizeof(HEADER);
+    if ((prevp = freep) == NULL) { // no free list yet
+        base.ptr = freep = prevp = &base;
         base.size = 0;
     }
 
-    for (p=q->ptr; ;q=p, p=p->ptr) {
+    for (p=prevp->ptr; ;prevp=p, p=p->ptr) {
         if (p->size >= nunits) { // big enough
             if (p->size == nunits) { // exactly
-                q->ptr = p->ptr;
-            } else {
+                prevp->ptr = p->ptr;
+            } else { // allocate tail end
                 p->size -= nunits;
                 p += p->size;
                 p->size = nunits;
             }
-            allocp = q;
-            return ((char *)(p+1));
+            freep = prevp;
+            return (void *)(p+1);
         }
-        if (p == allocp) { // wrapped around free list
+        if (p == freep) { // wrapped around free list
             if ((p = morecore(nunits)) == NULL) {
                 return NULL; // none left
             }
@@ -93,8 +99,10 @@ int main(int argc, char *argv[])
     void *ptr1, *ptr2;
     HEADER *p;
 
-    ptr1 = alloc(1);
-    ptr2 = alloc(1);
+    printf("sizeof(HEADER) = %d\n", sizeof(HEADER));
+
+    ptr1 = kr_malloc(3);
+    ptr2 = kr_malloc(1024);
     printf("ptr1: %p\n", ptr1);
     printf("ptr2: %p\n", ptr2);
 
@@ -102,11 +110,12 @@ int main(int argc, char *argv[])
     printf("ptr1 { next: %p, size: %u }\n", p->ptr, p->size);
     p = (HEADER *)ptr2 - 1;
     printf("ptr2 { next: %p, size: %u }\n", p->ptr, p->size);
-    printf("allocp { next: %p, size: %u }\n", allocp->ptr, allocp->size);
+    printf("allocp { ptr: %p, size: %u }\n", freep->ptr, freep->size);
+    printf("allocp->ptr { ptr: %p, size: %u }\n", freep->ptr->ptr, freep->ptr->size);
     printf("base { next: %p, size: %u }\n", base.ptr, base.size);
 
-    ffree(ptr1);
-    ffree(ptr2);
+    kr_free(ptr1);
+    kr_free(ptr2);
 
     return 0;
 }
